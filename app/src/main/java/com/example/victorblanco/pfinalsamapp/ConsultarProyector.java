06144696631acd65ea06906a2bc7f61f3561603b
Victor Blanco <victor.blanco@tectijuana.edu.mx>
2018-11-07 12:49:47 -0800

    Avance de Proyectores

--- app/src/main/java/com/example/victorblanco/pfinalsamapp/ConsultarProyector.java
+++ app/src/main/java/com/example/victorblanco/pfinalsamapp/ConsultarProyector.java
@@ -4,13 +4,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ConsultarProyector extends AppCompatActivity {
 
-    //Variable miembro
-    //Declaracion de constante
+
+    // Crear constante tag
+    private final static String TAG ="etiqueta";
+    private final static String KEY_INDEX="indice";
     private static final String SEND_MESSAGE="message";
     private static final String EXTRA_NAME_SHOWN="extraName";
+
+    //Creacion de variable miembro widgets
+    private TextView mProyectorTextView;
+    private Button mNextButton;
+    private Button mPreviousButton;
+
+    // Creacion de arreglo de estudiantes (MODELO)
+    private Proyector[] mProyectores = new Proyector[]{
+            new Proyector(111,"Epson",100,"Ocupado"),
+            new Proyector(222,"Acer",60,"Disponible"),
+            new Proyector(333,"Hitachi",95,"Disponible")
+    };
+
+    private int mCurrentIndex = 0;
+    // actualizacion de vista con el modelo
+    private void updateStudent(){
+        mProyectorTextView.setText(mProyectores[mCurrentIndex].getMarca() + "\n"
+                + mProyectores[mCurrentIndex].getModelo() + "\n"
+                + mProyectores[mCurrentIndex].getDisponible()
+        );
+    }
+
     //Asignacion del valor de la variable mensage a la constante
     //SEND_MESSAGE
     public static Intent newIntent(Context packageContext, String message){
@@ -28,5 +56,42 @@ public class ConsultarProyector extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_proyector);
 
+        // Inflar widgets (Conectar la VISTA con el CONTROLADOR)
+        mProyectorTextView = (TextView)findViewById(R.id.proyector_textView);
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mPreviousButton = (Button)findViewById(R.id.previous_button);
+
+        // Establecer listeners
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mCurrentIndex = (mCurrentIndex + 1) % (mProyectores.length);
+                updateStudent();
+            }
+        });
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mCurrentIndex = (mCurrentIndex+(mProyectores.length-1)) % mProyectores.length;
+                updateStudent();
+            }
+        });
+
+        if(savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
+        updateStudent();
+        Log.d(TAG,"onCreate() llamado");
+    }
+    // Guarda el valor de indice en KEY_INDEX
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG,"onSavedInstanceState() llamado");
+        savedInstanceState.putInt(KEY_INDEX,
+                mCurrentIndex);
     }
 }
--- app/src/main/java/com/example/victorblanco/pfinalsamapp/Proyector.java
+++ app/src/main/java/com/example/victorblanco/pfinalsamapp/Proyector.java
@@ -4,5 +4,45 @@ public class Proyector {
     private int id;
     private String marca;
     private int modelo;
-    private boolean disponible;
+    private String disponible;
+
+    public Proyector(int id, String marca, int modelo, String disponible) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(String disponible) {
+        this.disponible = disponible;
+    }
 }
+
